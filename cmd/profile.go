31bf6eccac99876d40be21ce5b6c30a06d34558f
e-mar404 <egmarinn@gmail.com>
2025-10-18 16:51:14 -0500

    add logo + join to profile

--- cmd/profile.go
+++ cmd/profile.go
@@ -2,29 +2,54 @@ package cmd
 
 import (
 	"fmt"
+	"strings"
 
+	"github.com/charmbracelet/lipgloss"
 	"github.com/charmbracelet/log"
 	"github.com/e-mar404/sgotify/api"
 	"github.com/spf13/cobra"
 )
 
+const logo string = 
+`⠀⠀⠀⠀⠀⠀⠀⢀⣠⣤⣤⣶⣶⣶⣶⣤⣤⣄⡀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⢀⣤⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣤⡀⠀⠀⠀⠀
+⠀⠀⠀⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦⠀⠀⠀
+⠀⢀⣾⣿⡿⠿⠛⠛⠛⠉⠉⠉⠉⠛⠛⠛⠿⠿⣿⣿⣿⣿⣿⣷⡀⠀
+⠀⣾⣿⣿⣇⠀⣀⣀⣠⣤⣤⣤⣤⣤⣀⣀⠀⠀⠀⠈⠙⠻⣿⣿⣷⠀
+⢠⣿⣿⣿⣿⡿⠿⠟⠛⠛⠛⠛⠛⠛⠻⠿⢿⣿⣶⣤⣀⣠⣿⣿⣿⡄
+⢸⣿⣿⣿⣿⣇⣀⣀⣤⣤⣤⣤⣤⣄⣀⣀⠀⠀⠉⠛⢿⣿⣿⣿⣿⡇
+⠘⣿⣿⣿⣿⣿⠿⠿⠛⠛⠛⠛⠛⠛⠿⠿⣿⣶⣦⣤⣾⣿⣿⣿⣿⠃
+⠀⢿⣿⣿⣿⣿⣤⣤⣤⣤⣶⣶⣦⣤⣤⣄⡀⠈⠙⣿⣿⣿⣿⣿⡿⠀
+⠀⠈⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣾⣿⣿⣿⣿⡿⠁⠀
+⠀⠀⠀⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠀⠀⠀
+⠀⠀⠀⠀⠈⠛⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠛⠁⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠈⠙⠛⠛⠿⠿⠿⠿⠛⠛⠋⠁⠀⠀⠀⠀⠀⠀⠀`
+
 var (
+	statsStyle = lipgloss.NewStyle().Padding(2)
+	logoStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("40"))
 	profileCmd  = &cobra.Command {
 		Use: "profile",
 		Short: "display some stats about your spotify profile",
 		RunE: func(cmd *cobra.Command, args []string) error {
-			fmt.Println("getting profile stuff...")
-
 			res, err := api.UserProfile()
 			if err != nil {
 				log.Error("could not get user profile", "error", err)
 				return err
 			}
-
 			log.Info("got back a user profile", "profile", res)
+			
+			stats := strings.Builder{}
+			stats.WriteString(fmt.Sprintf("Display Name: %s\n", res.DisplayName))
+			stats.WriteString(fmt.Sprintf("Email: %s\n", res.Email))
+			
+			output := lipgloss.JoinHorizontal(
+				lipgloss.Top,
+				logoStyle.Render(logo),
+				statsStyle.Render(stats.String()),
+			)
 
-			fmt.Printf("Display Name: %s\n", res.DisplayName)
-			fmt.Printf("Email: %s\n", res.Email)
+			fmt.Println(output)
 
 			return nil
 		},
