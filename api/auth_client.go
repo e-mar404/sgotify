12b8a3820566dce4ae89fb52918c4136a336f250
e-mar404 <egmarinn@gmail.com>
2025-10-25 21:13:25 -0500

    cmd {login}: retrieve access + refresh tokens

---
+++ api/auth_client.go
@@ -0,0 +1,39 @@
+package api
+
+import (
+	"encoding/base64"
+	"net/http"
+	"time"
+)
+
+type prepArgs struct {
+	ClientID     string
+	ClientSecret string
+}
+
+type AuthClient struct {
+	HTTP     *http.Client
+	prepArgs prepArgs
+	query    map[string]string
+}
+
+func NewAuthClient() *AuthClient {
+	return &AuthClient{
+		HTTP: &http.Client{
+			Timeout: 10 * time.Second,
+		},
+	}
+}
+
+func (ac *AuthClient) do(req *http.Request) (*http.Response, error) {
+	return ac.HTTP.Do(req)
+}
+
+func (ac *AuthClient) prep(req *http.Request) {
+	data := ac.prepArgs.ClientID + ":" + ac.prepArgs.ClientSecret
+	encodedData := base64.StdEncoding.EncodeToString([]byte(data))
+	authKey := "Basic " + encodedData
+
+	req.Header.Add("Authorization", authKey)
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+}
---
+++ api/auth_service.go
@@ -0,0 +1,68 @@
+package api
+
+import "github.com/charmbracelet/log"
+
+func init() {
+	server.Register(NewAuthService())
+}
+
+type LoginArgs struct {
+	ClientID     string
+	ClientSecret string
+	RedirectURI  string
+	BaseURL      string
+	Code         string
+	State        string
+}
+
+type LoginReply struct {
+	AccessToken  string `json:"access_token"`
+	RefreshToken string `json:"refresh_token"`
+}
+
+type Auth struct {
+	Client *AuthClient
+}
+
+func NewAuthService() *Auth {
+	return &Auth{
+		Client: NewAuthClient(),
+	}
+}
+
+func (a *Auth) LoginWithCode(args *LoginArgs, reply *LoginReply) error {
+	q := map[string]string{
+		"code":         args.Code,
+		"redirect_uri": args.RedirectURI,
+		"grant_type":   "authorization_code",
+	}
+	url := args.BaseURL + "/api/token"
+	a.Client.prepArgs = prepArgs{
+		ClientID:     args.ClientID,
+		ClientSecret: args.ClientSecret,
+	}
+
+	loginRes, err := do[LoginReply](a.Client, "POST", url, q)
+	if err != nil {
+		log.Error("unable to do LoginWithCode req", "error", err)
+		return err
+	}
+
+	*reply = *loginRes
+
+	return nil
+}
+
+// func (ac *AuthClient) RefreshAccessToken() (*LoginResponse, error) {
+// 	q := map[string]string{
+// 		"grant_type":    "refresh_token",
+// 		"refresh_token": viper.GetString("refresh_token"),
+// 	}
+// 	url := viper.GetString("spotify_account_url") + "/api/token"
+// 	refreshRes, err := do[LoginResponse](ac, "POST", url, q)
+// 	if err != nil {
+// 		log.Error("could not refresh access token", "error", err)
+// 		return nil, err
+// 	}
+// 	return refreshRes, nil
+// }
